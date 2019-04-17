2b0412600b0538e8f349a6506200aed9db5f5c4e
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-04-17 17:06:36 +0100

    Working parser, need to work on errors and integrate it properly

--- examples/QASMTokensPyparsing.py
+++ examples/QASMTokensPyparsing.py
@@ -1,15 +1,18 @@
 from pyparsing import *
 
+ParserElement.enablePackrat()
+
 cops = {}
 qops = {}
 blocks = {}
+reservedKeys = []
 
 class _Op:
 
     def __init__(self, name, argParser, version = "OPENQASM", qop = False, keyOverride = None):
         global cops
         global qops
-        
+        global reservedKeys
         if name in qops or name in cops: raise IOError(f'{name} already defined')
         self.operation = name
         if keyOverride is not None:
@@ -21,6 +24,7 @@ class _Op:
 
         self.version = version
 
+        reservedKeys.append(name)
         if qop:
             qops[name] = self
         else:
@@ -31,18 +35,19 @@ class _Routine(_Op):
                  returnable = False, prefixes = None, version = "OPENQASM"):
         global cops
         global qops
+        global reservedKeys
         if name in qops or name in cops: raise IOError(f'{name} already defined')
         self.operation = name
 
-        self.parser = Keyword(name)("keyword")
+        self.parser = Keyword(name)("keyword") + validName("gateName")
         
         if prefixes: self.parser = Each( map(Optional,prefixes) ) + self.parser
         
         # Handle different args
         req = []
-        if pargs: req.append(pargParser)
-        if spargs: req.append(spargParser)
-        if gargs: req.append(gargParser)
+        if pargs: req.append(Optional(pargParser))
+        if spargs: req.append(Optional(spargParser))
+        if gargs: req.append(Optional(gargParser))
         self.parser = self.parser + Each(req)
         if qargs: self.parser = self.parser + qargParser
         if returnable: self.parser = self.parser + returnParser
@@ -54,21 +59,13 @@ class _Block(_Op):
 
     def __init__(self, name, detParser, version = "OPENQASM"):
         global blocks
+        global reservedKeys
         self.operation = name
         self.parser = Keyword(name)("keyword") + detParser
+        reservedKeys.append(name)
         blocks[name] = self
 
         
-class _BlockMark():
-
-    delims = []
-    
-    def __init__(self, name, startDelim, endDelim, contents):
-
-        _Block.delims.append( (startDelim, endDelim) )
-        self.name = name
-        self.parser = nestedExpr(startDelim, endDelim, contents, (quotedString | comment))
-
     
 sign = Word("+-", exact=1)
 number = Word(nums)
@@ -78,11 +75,12 @@ e = CaselessKeyword("e")
 pi = CaselessKeyword("pi")
 
 integer  = Combine( number + Optional(expo))
-real = Combine( Optional(sign) + ( Combine("." + number) | Combine( number + "." + Optional(number) ) ) + Optional(expo))
+real = Combine( Optional(sign) + ( ("." + number) ^ ( number + "." + Optional(number) ) ) + Optional(expo))
 boolean = Word("TF", exact=1)
-validName = Combine( ~e + ~pi + Word(alphas,alphanums+"_") )
+validName = Forward()
 lineEnd = Literal(";")
 
+_is_ = Keyword("is").suppress()
 _in_ = Keyword("in").suppress()
 toClass = Literal("->").suppress()
 
@@ -91,8 +89,22 @@ realFunc = Or(intFunc, oneOf("arcsin arccos arctan sin cos tan exp ln sqrt"))
 
 inL,inS,inR = map(Suppress, "[:]")
 
-intExp  = infixNotation(
-    integer | validName,
+intExp = Forward()
+realExp = Forward()
+
+index = intExp.setResultsName('index')
+interval = Optional(intExp.setResultsName('start'), default=None) + inS + Optional(intExp.setResultsName('end'), default=None)
+ref = inL + delimitedList(index ^ interval) + inR
+regNoRef = validName("var")
+regRef   = Group(validName("var") + Optional(ref))
+regMustRef = Group(validName("var") + ref)
+regListNoRef = Group(delimitedList( regNoRef ))
+regListRef = Group(delimitedList( regRef ))
+regListMustRef = Group(delimitedList( regMustRef ))
+
+
+intExp << infixNotation(
+    integer ^ regRef,
     [
         (intFunc, 1, opAssoc.RIGHT),
         (oneOf("-")  , 1, opAssoc.RIGHT),
@@ -103,8 +115,8 @@ intExp  = infixNotation(
 )
 
 
-realExp = infixNotation(
-    integer | real | validName | pi | e,
+realExp << infixNotation(
+    real ^ integer ^ pi ^ e ^ regRef,
     [
         (realFunc, 1, opAssoc.RIGHT),
         (oneOf("-")  , 1, opAssoc.RIGHT),
@@ -115,7 +127,7 @@ realExp = infixNotation(
 )
 
 boolExp = infixNotation(
-    boolean | realExp | intExp,
+    boolean ^ realExp ^ intExp,
     [
         (oneOf("! not"), 1, opAssoc.RIGHT),
         (oneOf("and or xor"), 2, opAssoc.LEFT),
@@ -126,75 +138,120 @@ boolExp = infixNotation(
 
 mathExp = realExp ^ intExp ^ boolExp
 
-index = inL + intExp.setResultsName('index') + inR
-interval = inL + Optional(intExp.setResultsName('start'), default=None) + inS + Optional(intExp.setResultsName('end'), default=None) + inR
-ref = index ^ interval
 
 op  = []
 qop = []
 blockDelims = []
 
-procAttr = ["unitary"]
+procAttr = ["unitary recursive"]
 callMods = ["CTRL-", "INV-"] 
 
-
-regListNoRef = Group(delimitedList(validName))
-regListRef = Group(delimitedList( Group(validName + Optional(ref)) ))
-regListMustRef = Group(delimitedList( Group(validName + ref) ))
-
-pargParser = nestedExpr("(",")", delimitedList(realExp), None)("pargs")
-spargParser = nestedExpr("[","]", delimitedList(realExp), None)("spargs")
-gargParser = nestedExpr("<",">", delimitedList(validName), None)("gargs")
+pargParser = nestedExpr("(",")", delimitedList(realExp), None)
+spargParser = nestedExpr("[","]", delimitedList(realExp), None)
+gargParser = nestedExpr("<",">", delimitedList(validName), None)
 qargParser = regListRef("qargs")
 
 modifiers = Group(ZeroOrMore(oneOf(callMods)))
 attributes  = Each( map(Optional, map(Keyword, procAttr)) )("attributes")
 
-comment = Keyword("//").suppress() + restOfLine("comment")
+comment = Literal("//").suppress() + restOfLine("comment")
 
 _Op("directive", Word(alphanums)("directive") + CharsNotIn(";")("args"), version="REQASM", keyOverride = Keyword("**"))
 
-directiveBlock = originalTextFor(nestedExpr("** begin", "** end"))
+directiveBlock = originalTextFor(nestedExpr("**begin", "**end"))
 
 _Op("let", validName("var") + Literal("=").suppress() + mathExp("val"), version="REQASM")
 _Op("version", real("versionNumber"), version = None, keyOverride = Keyword("REQASM")^Keyword("OPENQASM") )
 _Op("include", quotedString("file").addParseAction(removeQuotes))
-_Op("opaque", validName("name") + regListNoRef("args"), keyOverride = attributes + "opaque")
+_Op("opaque", validName("name") + regListNoRef("qargs"), keyOverride = attributes + "opaque")
 _Routine("gate", pargs = True, qargs = True, prefixes = attributes)
 _Op("creg", validName("name") + Optional(index))
 _Op("qreg", validName("name") + Optional(index))
-_Op("call", Group(Each( map(Optional,[pargParser,spargParser,gargParser]) )) + regListRef,
-                        qop = True, keyOverride = (modifiers("mods") + validName("gate")) )
-_Op("measure", validName("qreg") + toClass + validName("creg"), qop = True)
+
+
+_Op("measure", regRef("qreg") + toClass + regRef("creg"), qop = True)
 _Op("barrier", regListNoRef("args"))
+_Op("alias", regRef("alias") + _is_ + regRef("target"), version = "REQASM")
+_Op("output", regRef("value"), version = "REQASM")
+
 _Block("for", validName("var") + _in_ + interval, version = "REQASM")
 _Block("if", "(" + boolExp("cond") + ")", version = "REQASM")
 _Block("while", "(" + boolExp("cond") + ")", version = "REQASM")
 
+callParser =  Optional(pargParser("pargs")) & Optional(spargParser("spargs")) & Optional(gargParser("gargs"))
+callGate = (modifiers("mods") + validName("gate")) + callParser + regListRef("qargs")
 
-qopsParsers = list(map ( lambda qop: qop.parser, qops.values()))
+
+qopsParsers = list(map ( lambda qop: qop.parser, qops.values())) + [callGate]
 blocksParsers = list(map ( lambda block: block.parser, blocks.values()))
 
-_Op("ifLine", blocks["for"].parser + Or(qopsParsers) , keyOverride = Keyword("if"))
-_Op("forLine", blocks["for"].parser + Or(qopsParsers), version="REQASM", keyOverride = Keyword("for"))
-_Op("whileLine", blocks["while"].parser + Or(qopsParsers), version="REQASM", keyOverride = Keyword("while"))
+_Op("if", blocks["if"].parser + Group(Or(qopsParsers))("Block"), keyOverride = Empty())
+_Op("for", blocks["for"].parser + Group(Or(qopsParsers))("Block"), version="REQASM", keyOverride = Empty())
+_Op("while", blocks["while"].parser + Group(Or(qopsParsers))("Block"), version="REQASM", keyOverride = Empty())
+
+reserved = Or(reservedKeys) ^ e ^ pi
+validName << (~reserved) + Word(alphas,alphanums+"_")
+
+print(validName)
 
 copsParsers = list(map ( lambda cop: cop.parser, cops.values()))
 
-operations = ( Or(copsParsers) ^ Or(qopsParsers) ) + lineEnd.suppress()
+operations = ( (Or(copsParsers) ^ Or(qopsParsers)) | callGate ) + lineEnd.suppress()
 
 validLine = Forward()
-codeBlock =  ungroup(nestedExpr("{","}", Group(validLine), (quotedString | comment)))
+codeBlock =  nestedExpr("{","}", Group(validLine), (quotedString | comment))
+
 validLine << (    (
     (operations + Optional(comment)) ^
     (Or(blocksParsers) + Optional(comment) + codeBlock("Block")) ^
             comment))                              # Whole line comment
-       
 
+testLine = (CharsNotIn("{;") + lineEnd) ^ (CharsNotIn("{") + codeBlock) ^ directiveBlock
+testKeyword = Word(alphas)("keyword")
 
+code = (Group(directiveBlock)) | Group(validLine)
 
-code = (directiveBlock) | validLine # ^ codeBlock
-
+currentLine = ""
 with open("test.qasm","r") as current:
-    for lines in code.searchString(current.read()):
-        print(lines.dump())
+    for line in current:
+        if not line.strip(): continue
+        currentLine += line
+        *test, = testLine.scanString(currentLine)
+        if not test or test[0][1] != 0: continue
+        try:
+            print(f"'{currentLine}'")
+            print(code.parseString(currentLine).dump())
+            currentLine = ""
+        except ParseException as err:
+            print(err.line)
+            print(" "*(err.column-1) + "^")
+            problem = testKeyword.parseString(err.line)
+            try:
+                if problem["keyword"] in qops.keys():
+                    qops[problem["keyword"]].parser.parseString(err.line)
+                elif problem["keyword"] in cops.keys():
+                    cops[problem["keyword"]].parser.parseString(err.line)
+                else:
+                    print("Unrecognised instruction: {} not found in {} format".format(problem["keyword"], "QASM"))
+            except ParseException as subErr:
+                print(subErr)
+            quit()
+    if currentLine:
+        try:
+            print(code.parseString(line).dump())
+        except ParseException as err:
+            print("Error : ")
+            print(err.line)
+            print(" "*(err.column-1) + "^")
+            #print(err)
+            quit()
+        
+    # for lines in code.searchString(current.read()):
+    #     print(lines.dump())
+    # try:
+    #     print(code.parseString(current.read(),parseAll=True).dump())
+    # except ParseException as err:
+    #     print(err.line)
+    #     print(" "*(err.column-1) + "^")
+    #     print(err)
+        
